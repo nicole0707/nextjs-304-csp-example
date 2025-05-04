fa4e3fa90e33563544e305cc678c9a229b731f77
nicole <nicole@nicoles-MacBook-Air.local>
2025-05-04 13:22:34 +1000

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    async headers() {
+      return [
+        {
+          source: '/trusted-lib.js',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "script-src 'self' https://cdn.example.com;",
+            },
+          ],
+        },
+      ];
+    }
 };
 
 export default nextConfig;
